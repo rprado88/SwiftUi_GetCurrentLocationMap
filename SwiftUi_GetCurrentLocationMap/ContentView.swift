049644ca37c22af2d5c0f191dc71f8d3f05eb520
Rodrigo Prado <32552146+rprado88@users.noreply.github.com>
2024-07-02 17:26:20 -0700

    Initial Commit

---
+++ SwiftUi_GetCurrentLocationMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi_GetCurrentLocationMap
+//
+//  Created by Rodrigo Prado on 02/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi_GetCurrentLocationMap/SwiftUi_GetCurrentLocationMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_GetCurrentLocationMapApp.swift
+//  SwiftUi_GetCurrentLocationMap
+//
+//  Created by Rodrigo Prado on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_GetCurrentLocationMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
